fcf27e2d09c812ac3327d8f636c173478508aef7
Sharad Ghimire <sharadghimire5551@gmail.com>
2020-05-24 19:49:40 +1000

    Add IE9 polyfill with core-js package

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
+/* polyfills.js */
+
+import "react-app-polyfill/ie9";
+import "react-app-polyfill/stable";
+import "core-js/features/array/find";
+import "core-js/features/array/includes";
+import "core-js/features/number/is-nan";
+
+/* index.js */
+
+import "./polyfills";
+
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
